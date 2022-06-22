436f70998a8ac987affa036f277e6ac121817827
João Gallego <joaogallegopolo@gmail.com>
2022-06-21 21:57:26 -0300

    Correção do Layout de Relatórios

--- src/app/components/Relatorios/relatorios/relatorios.component.ts
+++ src/app/components/Relatorios/relatorios/relatorios.component.ts
@@ -4,6 +4,11 @@ import { MatDialog } from '@angular/material/dialog';
 import { LoteService } from 'src/app/services/lote.service';
 import { RelatorioService } from 'src/app/services/relatorio.service';
 import { movimentacaoReport } from 'src/app/models/movimentacaoReport';
+import { animalReport } from 'src/app/models/animalReport';
+import { loteReport } from 'src/app/models/loteReport';
+import { MatRadioChange } from '@angular/material/radio';
+import { Raca } from 'src/app/models/raca';
+import { RacaService } from 'src/app/services/raca.service';
 
 @Component({
   selector: 'app-relatorios',
@@ -16,21 +21,57 @@ export class RelatoriosComponent implements OnInit {
     dataInicio: '',
     dataFim: '',
   }
+  animalReport: animalReport = {
+    RACA: '',
+  }
+  loteReport: loteReport = {
+    NRO_LOTE: '',
+  }
+
+  modelo: number = 0
+  racas: Raca[] = [];
 
   constructor(private RelatorioService: RelatorioService,
-              public dialog: MatDialog
+              public dialog: MatDialog,
+              private racaService: RacaService
               ) { }
 
   ngOnInit(): void {
+    this.findAllRaca();
+  }
+
+  findAllRaca(): void{
+    this.racaService.findAll().subscribe(resposta => {this.racas = resposta;});
   }
   
-
-
-
   imprimiRelatorio(){
+    if(this.modelo == 0){
+      console.log("Caiu aqui Imrpimi", this.modelo)
+      return this.RelatorioService.dowloadRelatorioMovimentacaoParam(this.movimentacaoReport);
+    }else if(this.modelo == 1){
+      console.log("Caiu aqui Imprimi", this.modelo)
+      return this.RelatorioService.dowloadRelatorioAnimal(this.animalReport);
+    }else if(this.modelo == 2){
+      console.log("Caiu aqui Imprimi Lote", this.modelo)
+      return this.RelatorioService.dowloadRelatorioLoteSem();
+    }
+    
+   return null;
+  }
+
+    limitaNumeros(e: any) {
+      let charCode = e.charCode ? e.charCode : e.keyCode;
+      // charCode 8 = backspace   
+      // charCode 9 = tab
+    
+      if (charCode != 8 && charCode != 9) {
+        // charCode 48 equivale a 0   
+        // charCode 57 equivale a 9
+      let max = 4;    
   
-    return this.RelatorioService.dowloadRelatorioParam(this.movimentacaoReport);
-  
+        if ((charCode < 48 || charCode > 57)||(e.target.value.length >= max)) return false;
+        else return true
+      }else return false
     }
 
 
--- src/app/services/relatorio.service.ts
+++ src/app/services/relatorio.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_CONFIG } from '../config/api.config';
 import { movimentacaoReport } from '../models/movimentacaoReport';
+import { animalReport } from 'src/app/models/animalReport';
+import { loteReport } from 'src/app/models/loteReport';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +12,37 @@ export class RelatorioService {
 
   constructor(private http: HttpClient) { }
 
-  dowloadRelatorio() {
+  dowloadRelatorioMovimentacao() {
     return this.http.get(`${API_CONFIG.baseUrl}/lote/relatorios`, {responseType: 'text'}).subscribe(data =>{
       document.querySelector('iframe').src = data;
     });
   }
 
-  dowloadRelatorioParam(movimentacaoReport: movimentacaoReport) {
+  dowloadRelatorioMovimentacaoParam(movimentacaoReport: movimentacaoReport) {
 
     return this.http.post(`${API_CONFIG.baseUrl}/lote/relatorios/`, movimentacaoReport, {responseType: 'text'}).subscribe(data =>{
       document.querySelector('iframe').src = data;
     });
   }
 
+  dowloadRelatorioAnimal(animalReport: animalReport) {
+
+    return this.http.post(`${API_CONFIG.baseUrl}/animalchip/relatorios/`, animalReport, {responseType: 'text'}).subscribe(data =>{
+      document.querySelector('iframe').src = data;
+    });
+  }
+
+  dowloadRelatorioLote(loteReport: loteReport) {
+
+    return this.http.post(`${API_CONFIG.baseUrl}/lote/relatorioLote/`, loteReport, {responseType: 'text'}).subscribe(data =>{
+      document.querySelector('iframe').src = data;
+    });
+  }
+
+  dowloadRelatorioLoteSem() {
+    return this.http.get(`${API_CONFIG.baseUrl}/lote/relatorioLoteSem`, {responseType: 'text'}).subscribe(data =>{
+      document.querySelector('iframe').src = data;
+    });
+  }
 
 }
